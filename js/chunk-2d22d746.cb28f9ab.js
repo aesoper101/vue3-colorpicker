ef2774b2772c64a7a44516b7ad34342aff49da85
aesoper <weilanzhuan@163.com>
2021-11-17 15:21:23 +0800

    Updates

--- js/chunk-2d22d746.cb28f9ab.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=t("7a23"),a={class:"about"},s=Object(c["g"])("h1",null,"This is an about page",-1);function u(n,e){return Object(c["r"])(),Object(c["e"])("div",a,[s])}const o={};o.render=u;e["default"]=o}}]);
